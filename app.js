17378e105349662c0c86cec97da227b6a4c0b908
Zihad Hossion <126801486+zihad-hossion@users.noreply.github.com>
2023-11-02 14:08:22 +0600

    done

---
+++ app.js
@@ -0,0 +1,39 @@
+const data = fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        return data;
+    }
+    )
+    .catch(error => console.log(error));
+
+// Convert data into array
+let chartContainer = document.querySelector('.chart-container');
+
+// Map through data and populate chart
+data.then(data => {
+    data.forEach(item => {
+        let chart = document.createElement('div');
+        chart.classList.add('chart');
+        const height = item.amount / 7;
+        let value = "";
+        if (item.day === "wed") {
+            value = `<article class="chart-wrapper">
+            <div class="chart-amount">$${item.amount}</div>
+            <div class="chart-value active" style="--height:${height}rem"></div>
+            </article>`;
+        } else {
+            value = `<article class="chart-wrapper">
+            <div class="chart-amount">$${item.amount}</div>
+            <div class="chart-value" style="--height:${height}rem"></div>
+            </article>`
+        }
+        chart.innerHTML = `
+        ${value}
+        <div class="chart-title">${item.day}</div>
+        `;
+        chartContainer.appendChild(chart);
+    }
+    )
+}
+)
